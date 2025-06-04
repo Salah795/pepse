551eef3e50964e76db7eee18a5ff126e30cc1182
Salah795 <salah.taher4@gmail.com>
2025-06-04 23:16:00 +0300

    arrived to 6.4.

--- pepse/world/Avatar.java
+++ pepse/world/Avatar.java
@@ -9,11 +9,16 @@ import java.awt.event.KeyEvent;
 
 public class Avatar extends GameObject {
     public static final int AVATAR_SIZE = 50;
+    private static final float IDLE_STATE_MODIFIER = 1;
+    private static final float RUN_STATE_MODIFIER = 0.5f;
+    private static final float JUMP_STATE_MODIFIER = 10;
     private static final float VELOCITY_X = 400;
     private static final float VELOCITY_Y = -650;
     private static final float GRAVITY = 600;
+    private static final float AVATAR_INITIAL_ENERGY = 100;
     private static final String AVATAR_IMAGE_PATH = "assets/idle_0.png";
 
+    private float avatarEnergy;
     private final UserInputListener inputListener;
 
     public Avatar(Vector2 topLeftCorner, UserInputListener inputListener, ImageReader imageReader) {
@@ -22,18 +27,28 @@ public class Avatar extends GameObject {
         physics().preventIntersectionsFromDirection(Vector2.ZERO);
         transform().setAccelerationY(GRAVITY);
         this.inputListener = inputListener;
+        this.avatarEnergy = AVATAR_INITIAL_ENERGY;
     }
 
     @Override
     public void update(float deltaTime) {
+        //TODO fix the implementation of this function.
         super.update(deltaTime);
         float xVel = 0;
-        if(inputListener.isKeyPressed(KeyEvent.VK_LEFT))
+        if(inputListener.isKeyPressed(KeyEvent.VK_LEFT) && this.avatarEnergy >= RUN_STATE_MODIFIER)
             xVel -= VELOCITY_X;
-        if(inputListener.isKeyPressed(KeyEvent.VK_RIGHT))
+        if(inputListener.isKeyPressed(KeyEvent.VK_RIGHT)&& this.avatarEnergy >= RUN_STATE_MODIFIER)
             xVel += VELOCITY_X;
+        if(xVel != 0)
+            this.avatarEnergy -= RUN_STATE_MODIFIER;
         transform().setVelocityX(xVel);
-        if(inputListener.isKeyPressed(KeyEvent.VK_SPACE) && getVelocity().y() == 0)
+        if(inputListener.isKeyPressed(KeyEvent.VK_SPACE) && getVelocity().y() == 0 &&
+                this.avatarEnergy >= JUMP_STATE_MODIFIER) {
             transform().setVelocityY(VELOCITY_Y);
+            this.avatarEnergy -= JUMP_STATE_MODIFIER;
+        }
+        if(getVelocity().equals(Vector2.ZERO) && this.avatarEnergy < AVATAR_INITIAL_ENERGY) {
+            this.avatarEnergy += IDLE_STATE_MODIFIER;
+        }
     }
 }
