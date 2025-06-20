f8ab39a58329e27614e36bc8c279213dd2dceda1
Salah795 <salah.taher4@gmail.com>
2025-06-20 16:46:26 +0300

    writing documentation

--- pepse/world/JumpObserver.java
+++ pepse/world/JumpObserver.java
@@ -1,6 +1,22 @@
 package pepse.world;
 
+/**
+ * The {@code JumpObserver} interface should be implemented by any object that wants to respond
+ * to avatar jump events.
+ * <p>
+ * Classes implementing this interface will receive a call to {@link #updateForJump()} whenever
+ * the avatar performs a jump, enabling interactive or animated behavior in response.
+ * <p>
+ * This is commonly used for elements like leaves, fruits, or other environment components that
+ * visually react when the avatar jumps.
+ * @author Salah Mahmied, Kais Sora.
+ */
 public interface JumpObserver {
-    //TODO check if the method should receive a parameter.
+
+    /**
+     * Called when the avatar jumps.
+     * Implementing classes should define what should happen (e.g., animate, reappear, etc.).
+     */
     void updateForJump();
 }
+
